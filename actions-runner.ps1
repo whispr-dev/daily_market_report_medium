7d0e673118a6206c05a24ec279663e1de13d6fed
whisprer <133140193+whisprer@users.noreply.github.com>
2025-03-25 07:22:41 +0000

    loops

--- actions-runner.ps1
+++ actions-runner.ps1
@@ -5,7 +5,7 @@ $runnerVersion = "2.322.0"
 $runnerZip = "actions-runner-win-x64-$runnerVersion.zip"
 $runnerUri = "https://github.com/actions/runner/releases/download/v$runnerVersion/$runnerZip"
 $expectedSHA256 = "ace5de018c88492ca80a2323af53ff3f43d2c82741853efb302928f250516015"
-$repoUrl = "https://github.com/whispr-dev" # Replace with your GitHub repository URL
+$repoUrl = "https://github.com/whispr-dev/daily_market_report_medium" # Replace with your GitHub repository URL
 $runnerToken = "A7XY5YIM5CT7XI4TO4P7DN3H4I2SC" # Replace with your actual runner token from GitHub
 $runnerDir = "$PWD\actions-runner"
 
@@ -35,7 +35,7 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 # Configure the runner
 Write-Host "Configuring runner..."
-& "$runnerDir\config.cmd" --url $"https://github.com/whispr-dev/daily_market_report_medium" --token $A7XY5YKMKLLY345ADD4DPHLH4JQTA --labels self-hosted
+& "$runnerDir\config.cmd" --url "https://github.com/whispr-dev/daily_market_report_medium" --token A7XY5YKNJWPTDAG3TFQT52DH4JRBY --labels self-hosted
 
 # Start the runner
 Write-Host "Starting the runner..."
